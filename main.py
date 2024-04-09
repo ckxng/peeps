af039411d0f0ad5a73d94ae3a43a2cfc0139fd70
Cameron King <cking@ckxng.com>
2024-04-08 21:48:33 -0500

    adds sensor data push to clients

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import argparse
 import asyncio
 import logging
 from functools import partial
+from typing import Optional
 
+import websockets
 from websockets.server import serve
 
 from lib.player import Player
@@ -13,15 +15,37 @@ from lib.region import Region
 logger = logging.getLogger('websockets')
 logger.addHandler(logging.StreamHandler())
 
+CONNECTIONS = set()
+WATCHERS = {}
+
 
 async def respond(websocket, region):
-    async for message in websocket:
-        if message == 'json':
-            await websocket.send(region.to_json())
-        elif message == 'compressed_json':
-            await websocket.send(region.to_compressed_json())
-        else:
-            await websocket.send(f"echo: {message}")
+    CONNECTIONS.add(websocket)
+    player: Optional[str] = None
+    try:
+        want_sensor_data: bool = False
+        async for message in websocket:
+            if player is None and message[:6] == 'login ':
+                player = message[6:]
+            elif player is not None:
+                if message == 'want_sensor_data':
+                    if player not in WATCHERS:
+                        WATCHERS[player] = set()
+                    WATCHERS[player].add(websocket)
+                    await websocket.send(f"watching {player} sensors")
+                elif message == 'no_want_sensor_data':
+                    WATCHERS[player].remove(websocket)
+                elif message == 'json':
+                    await websocket.send(region.to_json())
+                elif message == 'compressed_json':
+                    await websocket.send("compressed_sensor_data " + region.to_compressed_json())
+            else:
+                await websocket.send(f"invalid")
+        await websocket.wait_closed()
+    finally:
+        CONNECTIONS.remove(websocket)
+        if player is not None:
+            WATCHERS[player].remove(websocket)
 
 
 async def start_server(region: Region, host: str, port: int):
@@ -44,9 +68,15 @@ async def parse_args():
 
 
 async def simulate(region, timer):
+    step = 1
     while True:
-        region.step()
+        websockets.broadcast(CONNECTIONS, f"step {step}")
+        for player in WATCHERS:
+            for ws in WATCHERS[player]:
+                await ws.send("compressed_sensor_data " + region.to_compressed_json())
         await asyncio.sleep(timer)
+        region.step()
+        step += 1
 
 
 async def main():
